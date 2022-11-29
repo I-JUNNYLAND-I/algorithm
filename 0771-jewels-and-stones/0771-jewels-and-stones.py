97c77b2f3ed870e1f5e024c1ffdc04e8aa94f08e
QuiD-0 <wodnd101@naver.com>
2022-11-29 14:31:40 +0900

    Time: 53 ms (63.43%), Space: 13.9 MB (12.14%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,7 +1,12 @@
+from collections import defaultdict
+
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        ans = 0
+        stoneCount = defaultdict(int)
         for i in stones:
-            if i in jewels:
-                ans += 1
+            stoneCount[i] += 1
+        ans = 0
+        for i in jewels:
+            ans += stoneCount[i]
         return ans
+    
