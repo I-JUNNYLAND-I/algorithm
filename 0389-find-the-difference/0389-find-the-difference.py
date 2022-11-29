d6176b309dff98796580f63848ba96a2dd315b4d
QuiD-0 <wodnd101@naver.com>
2022-11-29 15:25:36 +0900

    Time: 41 ms (86.00%), Space: 13.9 MB (75.82%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,14 +1,5 @@
-from collections import defaultdict 
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        sdict = defaultdict(int)
-        tdict = defaultdict(int)
-        
-        for i in s:
-            sdict[i] += 1
-        for i in t:
-            tdict[i] += 1
-            
-        for i in tdict:
-            if tdict[i] != sdict[i]:
-                return i
+        return chr(sum(list(t.encode("ascii"))) - sum(list(s.encode("ascii"))))
+
+
