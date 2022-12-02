38858c83c1694fd9d573c62c833478ee28090564
Jinwoo An <84765181+wlsdn93@users.noreply.github.com>
2022-12-02 19:02:06 +0900

    Time: 220 ms (7.11%), Space: 101.4 MB (5.33%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        if (word1.length() != word2.length()) {
+            return false;
+        }
+        char[] chars1 = word1.toCharArray();
+        char[] chars2 = word2.toCharArray();
+        Arrays.sort(chars1);
+        Arrays.sort(chars2);
+        if (Arrays.equals(chars1, chars2)) {
+            return true;
+        }
+        Map<Character, Integer> map1 = getCharCountMap(chars1);
+        Map<Character, Integer> map2 = getCharCountMap(chars2);
+        int[] ints1 = map1.values().stream().mapToInt(Integer::intValue).toArray();
+        int[] ints2 = map2.values().stream().mapToInt(Integer::intValue).toArray();
+        Set<Character> set1 = map1.keySet();
+        Set<Character> set2 = map2.keySet();
+        Arrays.sort(ints1);
+        Arrays.sort(ints2);
+        return Arrays.equals(ints1, ints2) && set1.containsAll(set2);
+    }
+    
+    public Map<Character, Integer> getCharCountMap(char[] chars) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : chars) {
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        return map;
+    }
+}
