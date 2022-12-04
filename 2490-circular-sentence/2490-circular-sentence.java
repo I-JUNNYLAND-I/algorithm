794d9521f64bff8df380945649bf04474f8a20ca
Jinwoo An <84765181+wlsdn93@users.noreply.github.com>
2022-12-04 17:53:02 +0900

    Time: 3 ms (60.00%), Space: 42.9 MB (40.00%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isCircularSentence(String sentence) {
+        String[] strs = sentence.split(" ");
+        if (strs.length == 1) {
+            return strs[0].charAt(0) == strs[0].charAt(strs[0].length() - 1);
+        }
+        int len = strs.length;
+        char head = strs[0].charAt(0);
+        char tail = strs[0].charAt(strs[0].length() - 1);
+        for(int i = 0; i < len; i++) {
+            if (i == len - 1) {
+                if (head != tail) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+            if (strs[i + 1].charAt(0) == tail) {
+                tail = strs[i + 1].charAt(strs[i + 1].length() - 1);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
