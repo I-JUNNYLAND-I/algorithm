ef0ff906beb75da970d20c291037c542fba876ae
QuiD-0 <wodnd101@naver.com>
2022-12-09 17:17:53 +0900

    Time: 300 ms (75.35%), Space: 0B (100.00%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select id,
+case 
+    when id%2 = 0 then (select student from seat where id = (i.id-1) )  
+    when id%2 != 0 and id<(select count(student) from seat) then (select student from seat where id = (i.id+1) )  
+    else student
+end as student
+from seat i 
