fa488aa90867edc7362acbbc42faa165f142edec
Jinwoo An <84765181+wlsdn93@users.noreply.github.com>
2022-12-22 21:59:17 +0900

    Time: 20 ms (19.03%), Space: 43.1 MB (90.31%) - LeetHub

---
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int[] arr = new int[200001];
+    int maxFreq = 0;
+    public int[] findMode(TreeNode root) {
+        helper(root);
+        int max = 0;
+        int index = 0;
+        for (int num : arr) {
+            maxFreq = Math.max(maxFreq, num);
+        }
+        List<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == maxFreq) {
+                temp.add(i);
+            }
+        }
+        int[] answer = new int[temp.size()];
+        for (int i = 0; i < temp.size(); i++) {
+            answer[i] = temp.get(i) - 100000;
+        }
+        return answer;
+    }
+    
+    private void helper(TreeNode node) {
+        if (node == null) return;
+        arr[node.val + 100000]++;
+        helper(node.left);
+        helper(node.right);
+    }
+}
