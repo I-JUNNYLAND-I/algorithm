2080dc590926a2949e76f808fdbdcbf57573e67a
Jinwoo An <84765181+wlsdn93@users.noreply.github.com>
2022-12-23 12:50:06 +0900

    Time: 22 ms (60.15%), Space: 44.3 MB (77.16%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.java
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.java
@@ -2,11 +2,10 @@ import java.util.regex.Pattern;
 class Solution {
     public int countConsistentStrings(String allowed, String[] words) {
         String regex = String.format("^[%s]*$", allowed);
+        Pattern pattern = Pattern.compile(regex);
         int count = 0;
         for(String word : words) {
-            if (Pattern.matches(regex, word)) {
-                count++;
-            }
+            if (pattern.matcher(word).matches()) count++;
         }
         return count;
     }
