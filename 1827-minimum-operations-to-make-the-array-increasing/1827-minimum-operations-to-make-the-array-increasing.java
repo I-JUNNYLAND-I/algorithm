3aff8693b1a0b7e5ed62c1f92f86772a07d993f2
Jinwoo An <84765181+wlsdn93@users.noreply.github.com>
2022-12-28 14:43:49 +0900

    Time: 12 ms (46.64%), Space: 42.4 MB (96.07%) - LeetHub

---
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int count = 0;
+        int[] origin = Arrays.copyOf(nums, nums.length);
+        int[] temp = new int[nums.length];
+        temp[0] = nums[0];
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] >= nums[i + 1]) {
+                nums[i + 1] = nums[i] + 1;
+            }
+            temp[i + 1] = nums[i + 1];
+        }
+        System.out.println(Arrays.toString(temp));
+        for (int i = 0; i < origin.length; i++) {
+            count += (temp[i] - origin[i]);
+        }
+        return count;
+    }
+}
