83330c2076611b61e79929e5dbab5fa70285d197
Junny <72375574+Junnyjun@users.noreply.github.com>
2022-12-29 10:15:48 +0900

    Time: 271 ms (66.15%), Space: 39.1 MB (66.15%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.kt
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.kt
@@ -1,10 +1,10 @@
 class Solution {
-    fun answerQueries(nums: IntArray, queries: IntArray): IntArray {
-       nums.sort() 
-       for (i in 1..nums.lastIndex) nums[i] += nums[i-1]
-       return IntArray(queries.size) {
-           val ind = nums.binarySearch(queries[it])
-           if (ind < 0) -ind-1 else ind+1
-       }
+fun answerQueries(nums: IntArray, queries: IntArray): IntArray {
+    nums.sort()
+    for (i in 1..nums.lastIndex) nums[i] += nums[i-1]
+    return IntArray(queries.size) {
+        val search = nums.binarySearch(queries[it])
+        if (search < 0)-search-1 else search+1
     }
+}
 }
