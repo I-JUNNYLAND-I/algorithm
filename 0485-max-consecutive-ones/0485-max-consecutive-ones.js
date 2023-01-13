e783c6e894047d851f8cdc6c23a1838c4bea58cf
vjvl95 <62174495+vjvl95@users.noreply.github.com>
2023-01-13 19:10:42 +0900

    Time: 819 ms (5.04%), Space: 53.4 MB (5.17%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    let maxAppear=-1
+    let appear=0
+    let isContinue=false
+    for(let i=0;i<nums.length;i++){
+        console.log(nums[i],appear)
+        if(nums[i]===1){
+            isContinue=true
+            ++appear
+        }
+        else{
+            if(maxAppear<appear) maxAppear=appear
+            isContinue=false
+            appear=0
+        }
+    }
+    if(maxAppear<appear) maxAppear=appear
+     return maxAppear
+};
