190b5595c4275908aa63fb0061781dd894d992b4
vjvl95 <62174495+vjvl95@users.noreply.github.com>
2023-01-14 00:15:20 +0900

    Time: 100 ms (92.90%), Space: 53.1 MB (41.39%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.js
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.js
@@ -7,14 +7,12 @@ var countConsistentStrings = function(allowed, words) {
     let res=Array(words.length).fill(true)
     for(let i=0;i<words.length;i++){
         for(let j=0;j<words[i].length;j++){
-            console.log(words[i][j])
             if(allowed.indexOf(words[i][j])===-1){
               res[i]=false
             break;
             }
         }
     }
-    console.log(res)
     return res.filter((v)=>v!==false).length
     
 };
