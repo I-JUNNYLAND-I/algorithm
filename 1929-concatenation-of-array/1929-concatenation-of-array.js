d281685adbf30589be975665e75dc10d901b0321
vjvl95 <62174495+vjvl95@users.noreply.github.com>
2023-01-13 18:24:00 +0900

    Time: 94 ms (53.86%), Space: 46.1 MB (25.94%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    let res=[]
+    nums.forEach((v)=>{
+        res.push(v)
+    })
+     nums.forEach((v)=>{
+        res.push(v)
+    })
+    return res
+};
