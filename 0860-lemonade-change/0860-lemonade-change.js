c7aead5a4b2aac7d4b21813e0783f6c1f5d0ed59
vjvl95 <62174495+vjvl95@users.noreply.github.com>
2023-01-14 01:14:10 +0900

    Time: 92 ms (75.63%), Space: 50.4 MB (87.40%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.js
+++ 0860-lemonade-change/0860-lemonade-change.js
@@ -2,28 +2,24 @@
  * @param {number[]} bills
  * @return {boolean}
  */
-var lemonadeChange = function(bills) {
-    let change=[0,0,0]
-    
-    for(let i=0;i<bills.length;i++){
-        if(bills[i]===5) change[0]+=1
-        else if(bills[i]===10) {
-            if(change[0]>=1){
-                 change[1]+=1
-            change[0]-=1
-            }
-           
- else return false
-        }
-        else if(bills[i]===20){
-            if((change[0]>=1 && change[1]>=1)){
-            change[1]-=1
-            change[0]-=1
-            }else if(change[0]>=3){
-            change[0]-=3
-            }
-            else return false
-        }
+var lemonadeChange = function (bills) {
+  let change = [0, 0, 0];
+
+  for (let i = 0; i < bills.length; i++) {
+    if (bills[i] === 5) change[0] += 1;
+    else if (bills[i] === 10) {
+      if (change[0] >= 1) {
+        change[1] += 1;
+        change[0] -= 1;
+      } else return false;
+    } else if (bills[i] === 20) {
+      if (change[0] >= 1 && change[1] >= 1) {
+        change[1] -= 1;
+        change[0] -= 1;
+      } else if (change[0] >= 3) {
+        change[0] -= 3;
+      } else return false;
     }
-    return true
+  }
+  return true;
 };
