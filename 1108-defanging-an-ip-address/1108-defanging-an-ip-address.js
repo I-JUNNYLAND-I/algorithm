94c0f28e8384b2560c63b36adae19965f4b9ba31
vjvl95 <62174495+vjvl95@users.noreply.github.com>
2023-01-13 17:18:59 +0900

    Time: 54 ms (97.27%), Space: 41.7 MB (78.52%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
@@ -3,6 +3,13 @@
  * @return {string}
  */
 var defangIPaddr = function(address) {
-    address=address.split(".")
-    return address.join("[.]")
+ let result = '', length = address.length
+    for (let i = 0; i < length; i++) {
+        if (address[i] === '.') {
+            result += '[.]'
+        } else {
+            result += address[i]
+        }
+    }
+    return result
 };
