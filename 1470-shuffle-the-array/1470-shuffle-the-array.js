0e299a8bde787d5c3f787ad6f37e67436e1076af
vjvl95 <62174495+vjvl95@users.noreply.github.com>
2023-01-13 18:52:38 +0900

    Time: 67 ms (93.68%), Space: 44.4 MB (60.63%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.js
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -5,11 +5,10 @@
  */
 var shuffle = function(nums, n) {
     let res=[]
-    let yPoint=n
-    let xPoint=0
-    for(let i=0;i<nums.length;i++){
-       if(i%2===0) {res.push(nums[xPoint]); xPoint+=1}
-        if(i%2!==0) {res.push(nums[yPoint]); yPoint+=1}
+  
+    for(let i=0;i<n;i++){
+       res.push(nums[i])
+       res.push(nums[i+n])
     }
     return res
 };
