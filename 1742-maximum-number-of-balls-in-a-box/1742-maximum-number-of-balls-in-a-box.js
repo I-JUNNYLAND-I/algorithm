3558acac189a0180eb7ee2ee619626e2f6342940
vjvl95 <62174495+vjvl95@users.noreply.github.com>
2023-01-18 11:27:31 +0900

    Time: 95 ms (92.08%), Space: 42.4 MB (91.09%) - LeetHub

--- 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.js
+++ 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.js
@@ -9,8 +9,17 @@ var countBalls = function(lowLimit, highLimit) {
     let i=lowLimit
     
     while(i<=highLimit){
-        let tempNum=String(i).split("").map(Number).reduce((a,b)=>a+b)
-        baxCount[tempNum]+=1
+        let tempSum=0
+        let tempI=i
+        if(tempI<10)tempSum=i
+        else{
+            while(tempI>9){
+            tempSum+=tempI%10
+            tempI=Math.trunc(tempI/10);
+             }
+            tempSum+=tempI
+        }
+        baxCount[tempSum]+=1
         i++
     }
     return Math.max(...baxCount)
