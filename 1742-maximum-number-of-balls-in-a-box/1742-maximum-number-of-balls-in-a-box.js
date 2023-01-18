6855c801480bcce02e0811cad4ca38f715e090d1
vjvl95 <62174495+vjvl95@users.noreply.github.com>
2023-01-18 11:30:51 +0900

    Time: 90 ms (95.05%), Space: 42.2 MB (92.08%) - LeetHub

--- 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.js
+++ 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.js
@@ -3,24 +3,24 @@
  * @param {number} highLimit
  * @return {number}
  */
-var countBalls = function(lowLimit, highLimit) {
-    let baxCount=Array(46).fill(0)
-    
-    let i=lowLimit
-    
-    while(i<=highLimit){
-        let tempSum=0
-        let tempI=i
-        if(tempI<10)tempSum=i
-        else{
-            while(tempI>9){
-            tempSum+=tempI%10
-            tempI=Math.trunc(tempI/10);
-             }
-            tempSum+=tempI
-        }
-        baxCount[tempSum]+=1
-        i++
+var countBalls = function (lowLimit, highLimit) {
+  let baxCount = Array(46).fill(0);
+
+  let i = lowLimit;
+
+  while (i <= highLimit) {
+    let tempSum = 0;
+    let tempI = i;
+    if (tempI < 10) tempSum = i;
+    else {
+      while (tempI > 9) {
+        tempSum += tempI % 10;
+        tempI = Math.trunc(tempI / 10);
+      }
+      tempSum += tempI;
     }
-    return Math.max(...baxCount)
-    };
+    baxCount[tempSum] += 1;
+    i++;
+  }
+  return Math.max(...baxCount);
+};
