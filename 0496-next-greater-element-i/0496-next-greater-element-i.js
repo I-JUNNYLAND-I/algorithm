1c2001239907159d58ff3002ef147ce28745a7b9
vjvl95 <62174495+vjvl95@users.noreply.github.com>
2023-01-19 14:26:38 +0900

    Time: 76 ms (66.59%), Space: 42.6 MB (89.23%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+    let res=Array(nums1.length).fill(0)
+    for(let i=0;i<nums1.length;i++){
+        let index=nums2.indexOf(nums1[i])
+        for(let j=index;j<nums2.length;j++){
+            if(nums1[i]<nums2[j]) {
+                res[i]=nums2[j]
+                break;
+            }
+        }
+        if(res[i]===0) res[i]=-1
+    }
+    return res
+};
