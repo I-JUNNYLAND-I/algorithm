7c67223fd530926ffc3fe5c5878d03b5db0179cb
vjvl95 <62174495+vjvl95@users.noreply.github.com>
2023-01-19 18:04:02 +0900

    Time: 118 ms (62.65%), Space: 44.8 MB (53.09%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.js
+++ 0506-relative-ranks/0506-relative-ranks.js
@@ -5,10 +5,11 @@
 var findRelativeRanks = function(score) {
     let res=Array(score.length).fill(0)
     let tempStack=score.slice().sort((a,b)=>a-b)
-    
+    let maxNum=-1
+    let index=-1
     for(let i=0;i<score.length;i++){
-        let maxNum=tempStack.pop()
-        let index=score.indexOf(maxNum)
+        maxNum=tempStack.pop()
+        index=score.indexOf(maxNum)
         if(i<3){
             i===0?res[index]="Gold Medal":(i===1?res[index]="Silver Medal":res[index]="Bronze Medal")
         }
