44d2a47c86fb82191339ce7afb9e438623c2392e
vjvl95 <62174495+vjvl95@users.noreply.github.com>
2023-01-19 14:27:01 +0900

    Time: 68 ms (88.13%), Space: 42.7 MB (87.33%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -5,8 +5,9 @@
  */
 var nextGreaterElement = function(nums1, nums2) {
     let res=Array(nums1.length).fill(0)
+    let index=-1
     for(let i=0;i<nums1.length;i++){
-        let index=nums2.indexOf(nums1[i])
+        index=nums2.indexOf(nums1[i])
         for(let j=index;j<nums2.length;j++){
             if(nums1[i]<nums2[j]) {
                 res[i]=nums2[j]
