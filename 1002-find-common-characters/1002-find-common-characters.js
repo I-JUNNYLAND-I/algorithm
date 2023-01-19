10d6c827663878fad3b3502763f2f9b03ba7952d
vjvl95 <62174495+vjvl95@users.noreply.github.com>
2023-01-19 14:42:47 +0900

    Time: 141 ms (30.63%), Space: 50.9 MB (18.75%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var commonChars = function(words) {
+    let res=[]
+    let minCount=[]
+    let countWord=Array.from({length:words.length}, ()=>Array(26).fill(0))
+    
+    for(let i=0;i<words.length;i++){
+        for(let j=0;j<words[i].length;j++){
+            countWord[i][words[i][j].charCodeAt()-97]+=1
+        }
+    }
+    console.log(countWord)
+    for(let i=0;i<=26;i++){
+        for(let k=0;k<words.length;k++){
+            minCount.push(countWord[k][i])
+        }
+        let commonMin=Math.min(...minCount)
+        if(commonMin>=1){
+            for(let j=0;j<commonMin;j++){
+            res.push(String.fromCharCode(i+97))
+            }
+        }
+        minCount=[]
+    }
+    return res
+    
+};
