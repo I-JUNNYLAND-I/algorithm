e08d7a39191683d09719ae114d6cdaf6549d587a
vjvl95 <62174495+vjvl95@users.noreply.github.com>
2023-01-20 10:31:18 +0900

    Time: 250 ms (88.00%), Space: 69.2 MB (99.64%) - LeetHub

---
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+function reverseNum(num){
+    let tempStack=[]
+    if(num<10){
+        return num
+    }
+    while(num>=10){
+        tempStack.push(num%10)
+        num=Math.trunc(num/10)
+    }
+    tempStack.push(num)
+    return Number(tempStack.join(""))
+}
+var countDistinctIntegers = function(nums) {
+    let N=nums.length
+    let res=new Set(nums)
+    for(let i=0;i<N;i++){
+        let tempNum =reverseNum(nums[i])
+        res.add(tempNum)
+    }
+
+    return res.size
+};
+
