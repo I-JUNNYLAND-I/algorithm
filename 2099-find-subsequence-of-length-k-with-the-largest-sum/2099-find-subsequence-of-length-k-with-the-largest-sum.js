0f32f2e05dc47c5478566208b676c8a5d0c9acf8
vjvl95 <62174495+vjvl95@users.noreply.github.com>
2023-01-20 14:16:47 +0900

    Time: 80 ms (70.63%), Space: 43.9 MB (80.95%) - LeetHub

--- 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.js
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.js
@@ -5,6 +5,5 @@
  */
 var maxSubsequence = function(nums, k) {
     
-    let newArray=nums.map((v,i)=>[v,i]).sort((a,b)=>a[0]-b[0]).slice(nums.length-k)    
-    return newArray.sort((a,b)=> a[1]- b[1]).map((v)=>v[0])
+    return nums.map((v,i)=>[v,i]).sort((a,b)=>a[0]-b[0]).slice(nums.length-k).sort((a,b)=> a[1]- b[1]).map((v)=>v[0])    
     };
