c0421a45d8bc935a45130f51fb926693ca9a6126
vjvl95 <62174495+vjvl95@users.noreply.github.com>
2023-01-20 13:45:24 +0900

    Time: 68 ms (77.20%), Space: 44.1 MB (30.14%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
@@ -3,13 +3,8 @@
  * @return {boolean}
  */
 var checkIfExist = function(arr) {
-    let count=0
     if(arr.length-arr.filter((v)=>v!==0).length>=2) return true
-    let flag=false
-    
-    arr.forEach((v)=>{
-       
-    })
+
     for(let i=0; i<arr.length;i++){
          if(arr.indexOf(arr[i]*2)!==-1 && arr[i]!==0){
             return true
