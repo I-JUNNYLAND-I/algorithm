4e2608861c4a0c23f061a1b43085c6aab7c1de35
vjvl95 <62174495+vjvl95@users.noreply.github.com>
2023-01-22 22:26:07 +0900

    Time: 199 ms (11.19%), Space: 47.3 MB (5.15%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.js
+++ 0455-assign-cookies/0455-assign-cookies.js
@@ -8,20 +8,22 @@ var findContentChildren = function(g, s) {
 
     g.sort((a,b)=>a-b)
     s.sort((a,b)=>a-b)
-    let i=0
     let j=0
     let count=0
-    let iEnd=g.length;
-    let jEnd=s.length;
-    while(i<iEnd && j<jEnd ){
-        if(g[i]>s[j]){
-            j++
-        }
-        else{
-            i++
-            j++
-            count++
-        }
+
+    // while(i<iEnd && j<jEnd ){
+    //     if(g[i]>s[j]){
+    //         j++
+    //     }
+    //     else{
+    //         i++
+    //         j++
+    //         count++
+    //     }
+    // }
+    for(let num of s){
+        if(num>=g[j]) {j++
+                       count++}
     }
     return count
 };
