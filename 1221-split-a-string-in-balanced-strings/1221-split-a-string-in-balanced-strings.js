99cbd06f31585a7d26a6303050c39b00137eb5ac
vjvl95 <62174495+vjvl95@users.noreply.github.com>
2023-01-22 22:54:04 +0900

    Time: 102 ms (23.11%), Space: 41.3 MB (96.12%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
@@ -3,22 +3,20 @@
  * @return {number}
  */
 var balancedStringSplit = function(s) {
-    let Rcount=0;
+    let balace=0;
     let Lcount=0
     let splitCount=0;
     let i=0
     let sLength=s.length
     while(i<sLength){
         if(s[i]==='R'){
-            Rcount++
+            balace++
         }
         else{
-            Lcount++
+            balace--
         }
-        if(Rcount===Lcount){
+        if(balace===0){
             splitCount++
-            Rcount=0
-            Lcount=0
         }
         i++
     }
