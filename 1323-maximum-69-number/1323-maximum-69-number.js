29f2721beced1ac9fd7334a366046f4b9daf74f3
vjvl95 <62174495+vjvl95@users.noreply.github.com>
2023-01-22 23:04:40 +0900

    Time: 77 ms (38.81%), Space: 42 MB (57.14%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.js
+++ 1323-maximum-69-number/1323-maximum-69-number.js
@@ -6,7 +6,6 @@ var maximum69Number  = function(num) {
     num=num.toString().split("").map(Number)
     for(let i=0;i<4;i++){
         if(num[i]===6){
-            console.log(num[i])
             num[i]=9
             break;
         }
