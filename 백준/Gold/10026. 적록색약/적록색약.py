3368db6e1349deaee5647089691b4969cf1230a2
Jinwoo An <84765181+wlsdn93@users.noreply.github.com>
2023-02-08 09:06:47 +0900

    [Gold V] Title: 적록색약, Time: 112 ms, Memory: 31720 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,41 +1,44 @@
 import copy
 import sys
-sys.setrecursionlimit(10**6)
 
-def DFS(grid, row, col, target, target2):
-    if row >= 0 and col >= 0 and row < len(grid) and col < len(grid[0]) and (grid[row][col] == target or grid[row][col] == target2):
-        grid[row][col] = '-'
-        DFS(grid, row + 1, col, target, target2)
-        DFS(grid, row - 1, col, target, target2)
-        DFS(grid, row, col + 1, target, target2)
-        DFS(grid, row, col - 1, target, target2)
+sys.setrecursionlimit(10 ** 6)
+
+
+def dfs(grid, r, c, target, target2):
+    if 0 <= r < len(grid) and 0 <= c < len(grid[0]) and (grid[r][c] == target or grid[r][c] == target2):
+        grid[r][c] = '-'
+        dfs(grid, r + 1, c, target, target2)
+        dfs(grid, r - 1, c, target, target2)
+        dfs(grid, r, c + 1, target, target2)
+        dfs(grid, r, c - 1, target, target2)
+
 
 n = int(input())
-grid = []
+paint = []
 for row in range(n):
-    grid.append([*input()])
+    paint.append([*input()])
 
-grid_ = copy.deepcopy(grid)
+paint_ = copy.deepcopy(paint)
 count = 0
 R = 0
 G = 0
 B = 0
 for row in range(n):
     for col in range(n):
-        if grid[row][col] == 'R':
-            DFS(grid, row, col, "R", "R")
+        if paint[row][col] == 'R':
+            dfs(paint, row, col, "R", "R")
             R += 1
-        if grid[row][col] == 'G':
-            DFS(grid, row, col, "G", "G")
+        if paint[row][col] == 'G':
+            dfs(paint, row, col, "G", "G")
             G += 1
-        if grid[row][col] == 'B':
-            DFS(grid, row, col, "B", "B")
+        if paint[row][col] == 'B':
+            dfs(paint, row, col, "B", "B")
             B += 1
 RG = 0
 for row in range(n):
     for col in range(n):
-        if grid_[row][col] == 'R' or grid_[row][col] == "G":
-            DFS(grid_, row, col, "R", "G")
+        if paint_[row][col] == 'R' or paint_[row][col] == "G":
+            dfs(paint_, row, col, "R", "G")
             RG += 1
 
 print(R + G + B, RG + B)
