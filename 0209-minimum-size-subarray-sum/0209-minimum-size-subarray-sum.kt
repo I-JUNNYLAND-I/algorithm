2ea6a4ed3ff0e1bc3ecbcdb24e16ad9fa05499fb
QuiD-0 <wodnd101@naver.com>
2023-07-26 17:40:13 +0900

    Time: 337 ms (64.90%), Space: 48.4 MB (96.03%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun minSubArrayLen(target: Int, nums: IntArray): Int {
+        var sum = 0
+        var start = 0
+        var minLength = Integer.MAX_VALUE
+        var end = 0
+
+        if (nums.size < 1)
+            return 0
+
+        while(end < nums.size){
+            sum += nums[end]
+
+            while(sum >= target){
+                minLength = Math.min(minLength, end-start+1)
+                sum -= nums[start++]
+            }
+            end++
+        }
+        return if (start > 0) minLength else 0
+    }
+}
