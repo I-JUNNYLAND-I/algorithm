b7321ce79b3c06c835450181e7dc1fc6ad570bea
QuiD-0 <wodnd101@naver.com>
2023-08-01 15:00:46 +0900

    Time: 276 ms (54.55%), Space: 39.9 MB (59.09%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.kt
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.kt
@@ -1,19 +1,15 @@
 class Solution {
     fun groupThePeople(groupSizes: IntArray): List<List<Int>> {
+        val map = mutableMapOf<Int, MutableList<Int>>()
         val ans = mutableListOf<List<Int>>()
-        var sortedBy = IntRange(0, groupSizes.size-1)
-            .map { Group(it, groupSizes[it]) }
-            .sortedBy { it.value }
-        while (sortedBy.isNotEmpty()){
-            val count = sortedBy[0]
-            ans.add(sortedBy.subList(0,count.value).map { it.idx })
-            sortedBy = sortedBy.subList(count.value,sortedBy.size)
+        groupSizes.forEachIndexed { idx, it ->
+            var default = map.getOrDefault(it, mutableListOf()).also { it.add(idx) }
+            if (default.size == it){
+                ans.add(default)
+                default = mutableListOf()
+            }
+            map[it] = default
         }
         return ans
     }
 }
-
-data class Group(
-    val idx: Int,
-    val value: Int
-)
