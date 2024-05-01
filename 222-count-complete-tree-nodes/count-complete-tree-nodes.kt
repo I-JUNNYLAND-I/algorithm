674c0c597336e0322e9f6164c38f8059634ff6c8
QuiD-0 <wodnd101@naver.com>
2024-05-01 22:07:24 +0900

    Time: 206 ms (65.24%) | Memory: 39.6 MB (67.62%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.kt
@@ -0,0 +1,16 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun countNodes(root: TreeNode?): Int {
+        if (root == null) return 0
+        return 1 + countNodes(root.left) + countNodes(root.right)
+    }
+}
